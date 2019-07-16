2cb48e0aa48c20f5a0f4905aad6c2026dfe3f3ab
unknown <923772138@qq.com>
2019-07-16 11:44:47 +0800

    排版更新1

--- src/components/BrandList.vue
+++ src/components/BrandList.vue
@@ -1,28 +1,53 @@
 <template>
-    <div>
-        <ul v-for="(item, index) in data" :key="index">
-            <p>{{index}}</p>
-            <li v-for="(value) in item" :key="value.MasterID">
-                <img :src="value.CoverPhoto" :alt="value.Name">
-                <span>{{value.Name}}</span>
-            </li>
-        </ul>
-    </div>
+  <div>
+    <ul v-for="(item, index) in data" :key="index" class="ul">
+      <p>{{index}}</p>
+      <li v-for="(value) in item" :key="value.MasterID" class="main-li">
+        <img :src="value.CoverPhoto" :alt="value.Name" />
+        <span>{{value.Name}}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({
-    props: {
-        data: {
-            type: Object,
-            value: {}
-        }
+  props: {
+    data: {
+      type: Object,
+      value: {}
     }
-})
+  }
+});
 </script>
 
 
 <style lang="scss" scoped>
-
+.ul p {
+  height: 20px;
+  line-height: 20px;
+  color: #a9a9a9;
+  font-size: 14px;
+  padding-left: 16px;
+  background: #F4F4F4
+}
+.main-li {
+  height: 52px;
+  line-height: 52px;
+  background: white;
+  border-bottom: 1px solid #ccc;
+  padding-left: 16px;
+  display: flex;
+  align-items: center
+}
+.main-li img {
+  width: 38px;
+  height: 38px;
+}
+.main-li span{
+    display: inline-block;
+    padding-left: 20px;
+    font-size: 16px;
+}
 </style>
--- src/components/LetterList.vue
+++ src/components/LetterList.vue
@@ -1,37 +1,42 @@
 <template>
   <ul>
+    <li>#</li>
     <li v-for="(item) in data" :key="item">{{item}}</li>
   </ul>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  export default Vue.extend({
-    props: {
-      data: {
-        type: Array,
-        value: []
-      }
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    data: {
+      type: Array,
+      value: []
     }
-  })
+  }
+});
 </script>
 
 
 <style lang="scss" scoped>
-  ul {
-    position: fixed;
-    z-index: 101;
-    top: 0;
-    right: 0;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-  li {
-    color: #666;
-    font-weight: 500;
-    padding: .02rem .1rem;
-  }
+ul {
+  position: fixed;
+  z-index: 101;
+  top: 0;
+  right: 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-right: 4px
+}
+li {
+  color: #666;
+  font-weight: 500;
+  padding: 0.02rem 0.1rem;
+  font-size: 12px;
+  height: 16px;
+  line-height: 16px;
+}
 </style>
