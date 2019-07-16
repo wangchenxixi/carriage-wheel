d93cf2430c3014221626513189ae495dcac34825
unknown <923772138@qq.com>
2019-07-16 11:28:37 +0800

    框架搭建1

---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <BrandList :data="brandList"/> 
+    <LetterList :data="letterList"/> 
+
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import LetterList from '@/components/LetterList.vue'; // @ is an alias to /src
+import BrandList from '@/components/BrandList.vue'; // @ is an alias to /src
+import {mapActions, mapState} from 'vuex';
+
+export default Vue.extend({
+  name: 'home',
+  computed: {
+    ...mapState({
+      letterList: (state:any)=>state.home.letterList,
+      brandList: (state:any)=>state.home.brandList
+    })
+  },
+  methods: {
+    ...mapActions({
+      getBrandList: 'home/getBrandList'
+    })
+  },
+  created(){
+    this.getBrandList();
+  },
+  components: {
+    BrandList,
+    LetterList
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/global.scss';
+.home{
+  background-color: $page-background-color;
+  height: 100%;
+}
+</style>
+
